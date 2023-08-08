f3942e17aec7f283a2796eabbe4913726de4789a
Robert Santos <robertdosantos09@gmail.com>
2023-08-08 18:51:38 -0300

    feat: Adicionando todas as funcionalidades da pagina locations.

--- my-app/src/app/locations/page.tsx
+++ my-app/src/app/locations/page.tsx
@@ -1,7 +1,13 @@
+import { Nunito, DM_Sans, Roboto } from "next/font/google";
+
 import HeaderGlobal from "@/components/global/Header";
 import Item from "@/components/locations/Item";
 import { listAllLocations } from "@/services/api";
+import style from "../../styles/locations/page.module.css";
 
+const nunito = Nunito({ subsets: ['latin'], weight: ['800'] });
+const dmSans = DM_Sans({ subsets: ['latin'], weight: ['500'] });
+const roboto = Roboto({ subsets: ['latin'], weight: ['500'] });
 
 export default async function Locations() {
   const list = await listAllLocations();
@@ -9,10 +15,13 @@ export default async function Locations() {
   return (
     <>
       <HeaderGlobal title="Localizações" navigate="/" />
-      <main>
+      <main className={ style.main_locations }>
         <ul>
           { list.map((e: any) => (
             <Item
+              fontOne={ nunito }
+              fontTwo={ dmSans }
+              fontTree={ roboto }
               name= { e.name }
               type={ e.type }
               dimension={ e.dimension }
--- my-app/src/components/locations/Item.tsx
+++ my-app/src/components/locations/Item.tsx
@@ -1,36 +1,75 @@
+"use client";
+
 import { IoCopy } from "react-icons/io5";
+import copy from "copy-to-clipboard";
 
 import style from "../../styles/locations/page.module.css";
 
-type Props = {
-  name: string, type: string, dimension: string, residents: string[]
+interface IParams {
+  name: string,
+  type: string,
+  dimension: string,
+  residents: string[],
 }
 
-export default function Item(props: Props) {
+interface IProps extends IParams {
+  fontOne: { className: string },
+  fontTwo: { className: string },
+  fontTree: { className: string },
+}
+
+const copyClick = ({ name, type, dimension, residents }: IParams) => {
+  copy(JSON.stringify({
+      name,
+      type,
+      dimension,
+      residents: residents.length
+    }));
+};
+
+export default function Item(props: IProps) {
+  const { name, type, dimension, residents } = props;
+
   return (
     <li className={ style.li_item }>
       <div className={ style.div_info_location }>
-        <div><h3>{ props.name }</h3></div>
-        <div>
+        <div className={ style.div_info_name }>
+          <h3 className={ props.fontOne.className }>{ name }</h3>
+        </div>
+        <div className={ style.div_info_details }>
           <p>
-            <span>Tipo:</span>
+            <span className={ `${style.span_keys} ${props.fontTree.className}` }>
+              Tipo:
+            </span>
             {' '}
-            <span>{ props.type }</span>
+            <span className={ `${style.span_values} ${props.fontTwo.className}` }>
+              { type }
+            </span>
           </p>
           <p>
-            <span>Dimenção:</span>
+            <span className={ `${style.span_keys} ${props.fontTree.className}` }>
+              Dimenção:
+            </span>
             {' '}
-            <span>{ props.dimension }</span>
+            <span className={ `${style.span_values} ${props.fontTwo.className}` }>
+              { dimension === 'unknown' ? 'Desconhecido(a)' : dimension }
+            </span>
           </p>
           <p>
-            <span>Residentes:</span>
+            <span className={ `${style.span_keys} ${props.fontTree.className}` }>
+              Residentes:
+            </span>
             {' '}
-            <span>{ props.residents.length }</span>
+            <span className={ `${style.span_values} ${props.fontTwo.className}` }>
+              { residents.length }
+            </span>
           </p>
         </div>
       </div>
       <div className={ style.div_button_copy }>
-        <button>
+        <button onClick={ () => {
+          copyClick({ name, type, dimension, residents })
+        } }>
           <IoCopy />
         </button>
       </div>

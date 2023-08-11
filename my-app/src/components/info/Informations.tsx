e775becdfe075246ffc6092c15cf3b70b0d7552e
Robert Santos <robertdosantos09@gmail.com>
2023-08-11 18:23:59 -0300

    feat: Adicionando os estilos e funcionalidades finais da pagina "/info".

--- my-app/src/components/info/Informations.tsx
+++ my-app/src/components/info/Informations.tsx
@@ -1,33 +1,60 @@
+import { Nunito, Roboto } from "next/font/google";
+import Link from "next/link";
+
 import { LuCalendarCheck2 } from "react-icons/lu";
 import { BiSolidUserPin } from "react-icons/bi";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
 
+import style from "../../styles/info/page.module.css";
+
+
+const nunito = Nunito({ subsets: ['latin-ext'] });
+const roboto = Roboto({ subsets: ['latin'], weight: ['400'] });
+
 export default function Informations() {
   return (
-    <main>
-      <div></div>
-      <div>
-        <div><h3>Robert Santos</h3></div>
-        <div>
+    <main className={ style.main_info }>
+      <div className={ style.div_img_autor }></div>
+      <div className={ style.div_info_autor }>
+        <div className={ style.div_name_autor }>
+          <h3 className={ nunito.className }>Robert Santos</h3>
+        </div>
+        <div className={ style.div_details_autor }>
           <p>
-            <span><LuCalendarCheck2 />:</span>
+            <span className={ style.span_icons }><LuCalendarCheck2 /></span>
             {' '}
-            <span>23 anos</span>
+            <span className={ `${roboto.className} ${style.span_info}` }>
+              23 anos
+            </span>
           </p>
           <p>
-            <span><BiSolidUserPin />:</span>
+            <span className={ style.span_icons }><BiSolidUserPin /></span>
             {' '}
-            <span>Desenvolvedor Web FullStack</span>
+            <span className={ `${roboto.className} ${style.span_info}` }>
+              Desenvolvedor Web FullStack
+            </span>
           </p>
           <p>
-            <span><BsGithub />:</span>
+            <span className={ style.span_icons }><BsGithub /></span>
             {' '}
-            <span>gitHub link</span>
+            <Link
+              href="https://github.com/RobertSantos-dev"
+              target="_blank"
+              className={ `${roboto.className} ${style.link_info}` }
+            >
+              github.com/RobertSantos-dev
+            </Link>
           </p>
           <p>
-            <span><BsLinkedin />:</span>
+            <span className={ style.span_icons }><BsLinkedin /></span>
             {' '}
-            <span>Linkedin link</span>
+            <Link
+              href="https://www.linkedin.com/in/robert-santos-dev/"
+              target="_blank"
+              className={ `${roboto.className} ${style.link_info}` }
+            >
+              linkedin.com/in/robert-santos-dev/
+            </Link>
           </p>
         </div>
       </div>
